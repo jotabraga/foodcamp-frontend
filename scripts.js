4f16837c627bc3821cd4826f6bf01564039866ba
jotabraga <jpmbraga.feg@gmail.com>
2021-04-02 13:15:01 -0300

    Correção bug seleção de sobremesas

--- scripts.js
+++ scripts.js
@@ -37,6 +37,54 @@ function selectdish(plate){
     close_order();
 }
 
+function selectdrink(beverage){
+    
+    const selected = document.querySelector(".drink-menu .drink-selected");
+    const ic = document.querySelector(".drink-selected" + " ion-icon");
+    
+
+    if(selected !== null){
+        selected.classList.remove('drink-selected');
+        ic.classList.replace("icon-check","hidden");
+    }
+
+    beveragechoosed = document.querySelector(beverage);
+    beveragechoosed.classList.add("drink-selected");
+    const icon = document.querySelector(beverage + " ion-icon");
+    icon.classList.replace("hidden","icon-check");
+
+    const meal = document.querySelector(".drink-selected .main-drink");
+    drinkchoosed = meal.innerHTML
+
+    const bill = document.querySelector(".drink-selected .price");
+    drinkprice = bill.innerHTML
+    close_order();
+}
+
+function selectdessert(sweets){
+    
+    const selected = document.querySelector(".dessert-menu .dessert-selected");
+    const ic = document.querySelector(".dessert-selected" + " ion-icon");
+    
+
+    if(selected !== null){
+        selected.classList.remove('dessert-selected');
+        ic.classList.replace("icon-check","hidden");
+    }
+
+    sweetchoosed = document.querySelector(sweets);
+    sweetchoosed.classList.add("dessert-selected");
+    const icon = document.querySelector(sweets + " ion-icon");
+    icon.classList.replace("hidden","icon-check");
+
+    const meal = document.querySelector(".dessert-selected .main-dessert");
+    dessertchoosed = meal.innerHTML
+
+    const bill = document.querySelector(".dessert-selected .price");
+    dessertprice = bill.innerHTML
+    close_order();
+}
+
 
 
 
